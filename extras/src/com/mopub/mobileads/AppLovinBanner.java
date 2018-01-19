813928760d2b63ffd18b1a741b211a339f4904e9
Thomas So <thomas.m.so33@gmail.com>
2018-01-18 16:28:13 -0800

    Add MoPub 2.1.0 adapters

---
+++ extras/src/com/mopub/mobileads/AppLovinBanner.java
@@ -0,0 +1,299 @@
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import com.applovin.adview.AppLovinAdView;
+import com.applovin.adview.AppLovinAdViewEventListener;
+import com.applovin.sdk.AppLovinAd;
+import com.applovin.sdk.AppLovinAdClickListener;
+import com.applovin.sdk.AppLovinAdDisplayListener;
+import com.applovin.sdk.AppLovinAdLoadListener;
+import com.applovin.sdk.AppLovinAdSize;
+import com.applovin.sdk.AppLovinErrorCodes;
+import com.applovin.sdk.AppLovinSdk;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
+import static android.util.Log.DEBUG;
+import static android.util.Log.ERROR;
+
+/**
+ * AppLovin SDK banner adapter for MoPub.
+ * <p>
+ * Created by Thomas So on 3/6/17.
+ */
+
+public class AppLovinBanner
+        extends CustomEventBanner
+{
+    private static final boolean LOGGING_ENABLED = true;
+
+    private static final int BANNER_STANDARD_HEIGHT         = 50;
+    private static final int BANNER_HEIGHT_OFFSET_TOLERANCE = 10;
+
+    private static final String AD_WIDTH_KEY  = "com_mopub_ad_width";
+    private static final String AD_HEIGHT_KEY = "com_mopub_ad_height";
+
+    //
+    // MoPub Custom Event Methods
+    //
+
+    @Override
+    protected void loadBanner(final Context context, final CustomEventBannerListener customEventBannerListener, final Map<String, Object> localExtras, final Map<String, String> serverExtras)
+    {
+        // SDK versions BELOW 7.1.0 require a instance of an Activity to be passed in as the context
+        if ( AppLovinSdk.VERSION_CODE < 710 && !( context instanceof Activity ) )
+        {
+            log( ERROR, "Unable to request AppLovin banner. Invalid context provided." );
+            customEventBannerListener.onBannerFailed( MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR );
+
+            return;
+        }
+
+        log( DEBUG, "Requesting AppLovin banner with localExtras: " + localExtras );
+
+        final AppLovinAdSize adSize = appLovinAdSizeFromLocalExtras( localExtras );
+        if ( adSize != null )
+        {
+            final AppLovinSdk sdk = AppLovinSdk.getInstance( context );
+            sdk.setPluginVersion( "MoPub-Certified-2.1.0" );
+
+            final AppLovinAdView adView = createAdView( adSize, serverExtras, context, customEventBannerListener );
+
+            adView.setAdLoadListener( new AppLovinAdLoadListener()
+            {
+                @Override
+                public void adReceived(final AppLovinAd ad)
+                {
+                    log( DEBUG, "Successfully loaded banner ad" );
+                    customEventBannerListener.onBannerLoaded( adView );
+                }
+
+                @Override
+                public void failedToReceiveAd(final int errorCode)
+                {
+                    log( ERROR, "Failed to load banner ad with code: " + errorCode );
+                    customEventBannerListener.onBannerFailed( toMoPubErrorCode( errorCode ) );
+
+                    // TODO: Add support for backfilling on regular ad request if invalid zone entered
+                }
+            } );
+            adView.setAdDisplayListener( new AppLovinAdDisplayListener()
+            {
+                @Override
+                public void adDisplayed(final AppLovinAd ad)
+                {
+                    log( DEBUG, "Banner displayed" );
+                }
+
+                @Override
+                public void adHidden(final AppLovinAd ad)
+                {
+                    log( DEBUG, "Banner dismissed" );
+                }
+            } );
+            adView.setAdClickListener( new AppLovinAdClickListener()
+            {
+                @Override
+                public void adClicked(final AppLovinAd ad)
+                {
+                    log( DEBUG, "Banner clicked" );
+
+                    customEventBannerListener.onBannerClicked();
+                    customEventBannerListener.onLeaveApplication();
+                }
+            } );
+
+            // As of Android SDK >= 7.3.0, we added a listener for banner events
+            if ( AppLovinSdk.VERSION_CODE >= 730 )
+            {
+                adView.setAdViewEventListener( (AppLovinAdViewEventListener) AppLovinAdViewEventListenerProxy.newInstance( customEventBannerListener ) );
+            }
+
+            adView.loadNextAd();
+        }
+        else
+        {
+            log( ERROR, "Unable to request AppLovin banner" );
+
+            customEventBannerListener.onBannerFailed( MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR );
+        }
+    }
+
+    @Override
+    protected void onInvalidate() {}
+
+    //
+    // Utility Methods
+    //
+
+    private AppLovinAdSize appLovinAdSizeFromLocalExtras(final Map<String, Object> localExtras)
+    {
+        // Handle trivial case
+        if ( localExtras == null || localExtras.isEmpty() )
+        {
+            log( ERROR, "No serverExtras provided" );
+            return null;
+        }
+
+        try
+        {
+            final int width = (Integer) localExtras.get( AD_WIDTH_KEY );
+            final int height = (Integer) localExtras.get( AD_HEIGHT_KEY );
+
+            // We have valid dimensions
+            if ( width > 0 && height > 0 )
+            {
+                log( DEBUG, "Valid width (" + width + ") and height (" + height + ") provided" );
+
+                // Assume fluid width, and check for height with offset tolerance
+                final int offset = Math.abs( BANNER_STANDARD_HEIGHT - height );
+
+                if ( offset <= BANNER_HEIGHT_OFFSET_TOLERANCE )
+                {
+                    return AppLovinAdSize.BANNER;
+                }
+                else if ( height <= AppLovinAdSize.MREC.getHeight() )
+                {
+                    return AppLovinAdSize.MREC;
+                }
+                else
+                {
+                    log( ERROR, "Provided dimensions does not meet the dimensions required of banner or mrec ads" );
+                }
+            }
+            else
+            {
+                log( ERROR, "Invalid width (" + width + ") and height (" + height + ") provided" );
+            }
+        }
+        catch ( Throwable th )
+        {
+            log( ERROR, "Encountered error while parsing width and height from serverExtras", th );
+        }
+
+        return null;
+    }
+
+    //
+    // Utility Methods
+    //
+
+    private AppLovinAdView createAdView(final AppLovinAdSize size, final Map<String, String> serverExtras, final Context parentContext, final CustomEventBannerListener customEventBannerListener)
+    {
+        AppLovinAdView adView = null;
+
+        try
+        {
+            // AppLovin SDK < 7.1.0 uses an Activity, as opposed to Context in >= 7.1.0
+            final Class<?> contextClass = ( AppLovinSdk.VERSION_CODE < 710 ) ? Activity.class : Context.class;
+
+            // Zones support is available on AppLovin SDK 7.5.0 and higher
+            final Constructor<?> constructor;
+            if ( AppLovinSdk.VERSION_CODE >= 750 && serverExtras != null && serverExtras.containsKey( "zone_id" ) )
+            {
+                // Dynamically create an instance of AppLovinAdView with a given zone without breaking backwards compatibility for publishers on older SDKs.
+                constructor = AppLovinAdView.class.getConstructor( AppLovinAdSize.class, String.class, contextClass );
+                adView = (AppLovinAdView) constructor.newInstance( size, serverExtras.get( "zone_id" ), parentContext );
+            }
+            else
+            {
+                constructor = AppLovinAdView.class.getConstructor( AppLovinAdSize.class, contextClass );
+                adView = (AppLovinAdView) constructor.newInstance( size, parentContext );
+            }
+        }
+        catch ( Throwable th )
+        {
+            log( ERROR, "Unable to get create AppLovinAdView." );
+            customEventBannerListener.onBannerFailed( MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR );
+        }
+
+        return adView;
+    }
+
+    private static void log(final int priority, final String message)
+    {
+        log( priority, message, null );
+    }
+
+    private static void log(final int priority, final String message, final Throwable th)
+    {
+        if ( LOGGING_ENABLED )
+        {
+            Log.println( priority, "AppLovinBanner", message + ( ( th == null ) ? "" : Log.getStackTraceString( th ) ) );
+        }
+    }
+
+    private static MoPubErrorCode toMoPubErrorCode(final int applovinErrorCode)
+    {
+        if ( applovinErrorCode == AppLovinErrorCodes.NO_FILL )
+        {
+            return MoPubErrorCode.NETWORK_NO_FILL;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.UNSPECIFIED_ERROR )
+        {
+            return MoPubErrorCode.NETWORK_INVALID_STATE;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.NO_NETWORK )
+        {
+            return MoPubErrorCode.NO_CONNECTION;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.FETCH_AD_TIMEOUT )
+        {
+            return MoPubErrorCode.NETWORK_TIMEOUT;
+        }
+        else
+        {
+            return MoPubErrorCode.UNSPECIFIED;
+        }
+    }
+
+    /**
+     * Dynamic proxy class for AppLovin's AppLovinAdViewEventListener. Used to keep compilation compatibility if publisher is on a version of the SDK before the listener was introduced (< 7.3.0).
+     */
+    private static final class AppLovinAdViewEventListenerProxy
+            implements InvocationHandler
+    {
+        private final CustomEventBannerListener customEventBannerListener;
+
+        private static Object newInstance(final CustomEventBannerListener customEventBannerListener)
+        {
+            return Proxy.newProxyInstance( AppLovinAdViewEventListener.class.getClassLoader(),
+                                           new Class[] { AppLovinAdViewEventListener.class },
+                                           new AppLovinAdViewEventListenerProxy( customEventBannerListener ) );
+        }
+
+        private AppLovinAdViewEventListenerProxy(final CustomEventBannerListener customEventBannerListener)
+        {
+            this.customEventBannerListener = customEventBannerListener;
+        }
+
+        public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable
+        {
+            final String methodName = method.getName();
+
+            if ( "adOpenedFullscreen".equals( methodName ) )
+            {
+                log( DEBUG, "Banner opened fullscreen" );
+                customEventBannerListener.onBannerExpanded();
+            }
+            else if ( "adClosedFullscreen".equals( methodName ) )
+            {
+                log( DEBUG, "Banner closed fullscreen" );
+                customEventBannerListener.onBannerCollapsed();
+            }
+            else if ( "adLeftApplication".equals( methodName ) )
+            {
+                // We will fire onLeaveApplication() in the adClicked() callback
+                log( DEBUG, "Banner left application" );
+            }
+            else if ( "adFailedToDisplay".equals( methodName ) ) {}
+
+            return null;
+        }
+    }
+}
---
+++ extras/src/com/mopub/mobileads/AppLovinRewardedVideo.java
@@ -0,0 +1,321 @@
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.applovin.adview.AppLovinIncentivizedInterstitial;
+import com.applovin.sdk.AppLovinAd;
+import com.applovin.sdk.AppLovinAdClickListener;
+import com.applovin.sdk.AppLovinAdDisplayListener;
+import com.applovin.sdk.AppLovinAdLoadListener;
+import com.applovin.sdk.AppLovinAdRewardListener;
+import com.applovin.sdk.AppLovinAdVideoPlaybackListener;
+import com.applovin.sdk.AppLovinErrorCodes;
+import com.applovin.sdk.AppLovinSdk;
+import com.mopub.common.LifecycleListener;
+import com.mopub.common.MoPubReward;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.util.Log.DEBUG;
+import static android.util.Log.ERROR;
+
+/**
+ * AppLovin SDK rewarded video adapter for MoPub.
+ * <p>
+ * Created by Thomas So on 5/27/17.
+ */
+
+public class AppLovinRewardedVideo
+        extends CustomEventRewardedVideo
+        implements AppLovinAdLoadListener, AppLovinAdDisplayListener, AppLovinAdClickListener, AppLovinAdVideoPlaybackListener, AppLovinAdRewardListener
+{
+    private static final boolean LOGGING_ENABLED = true;
+    private static final String  DEFAULT_ZONE    = "";
+
+    // A map of Zone -> `AppLovinIncentivizedInterstitial` to be shared by instances of the custom event.
+    // This prevents skipping of ads as this adapter will be re-created and preloaded (along with underlying `AppLovinIncentivizedInterstitial`)
+    // on every ad load regardless if ad was actually displayed or not.
+    private static final Map<String, AppLovinIncentivizedInterstitial> GLOBAL_INCENTIVIZED_INTERSTITIAL_ADS = new HashMap<String, AppLovinIncentivizedInterstitial>();
+
+    private static boolean initialized;
+
+    private AppLovinIncentivizedInterstitial incentivizedInterstitial;
+    private Activity                         parentActivity;
+
+    private boolean     fullyWatched;
+    private MoPubReward reward;
+
+
+    //
+    // MoPub Custom Event Methods
+    //
+
+    @Override
+    protected boolean checkAndInitializeSdk(@NonNull final Activity activity, @NonNull final Map<String, Object> localExtras, @NonNull final Map<String, String> serverExtras) throws Exception
+    {
+        log( DEBUG, "Initializing AppLovin rewarded video..." );
+
+        if ( !initialized )
+        {
+            AppLovinSdk.initializeSdk( activity );
+            AppLovinSdk.getInstance( activity ).setPluginVersion( "MoPub-Certified-2.1.0" );
+
+            initialized = true;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void loadWithSdkInitialized(@NonNull final Activity activity, @NonNull final Map<String, Object> localExtras, @NonNull final Map<String, String> serverExtras) throws Exception
+    {
+        log( DEBUG, "Requesting AppLovin rewarded video with serverExtras: " + serverExtras );
+
+        parentActivity = activity;
+
+        // Zones support is available on AppLovin SDK 7.5.0 and higher
+        final String zoneId;
+        if ( AppLovinSdk.VERSION_CODE >= 750 && serverExtras != null && serverExtras.containsKey( "zone_id" ) )
+        {
+            zoneId = serverExtras.get( "zone_id" );
+        }
+        else
+        {
+            zoneId = DEFAULT_ZONE;
+        }
+
+
+        // Check if incentivized ad for zone already exists
+        if ( GLOBAL_INCENTIVIZED_INTERSTITIAL_ADS.containsKey( zoneId ) )
+        {
+            incentivizedInterstitial = GLOBAL_INCENTIVIZED_INTERSTITIAL_ADS.get( zoneId );
+        }
+        else
+        {
+            // If this is a default Zone, create the incentivized ad normally
+            if ( DEFAULT_ZONE.equals( zoneId ) )
+            {
+                incentivizedInterstitial = AppLovinIncentivizedInterstitial.create( activity );
+            }
+            // Otherwise, use the Zones API
+            else
+            {
+                incentivizedInterstitial = createIncentivizedInterstitialForZoneId( zoneId, AppLovinSdk.getInstance( activity ) );
+            }
+
+            GLOBAL_INCENTIVIZED_INTERSTITIAL_ADS.put( zoneId, incentivizedInterstitial );
+        }
+
+        incentivizedInterstitial.preload( this );
+    }
+
+    @Override
+    protected void showVideo()
+    {
+        if ( hasVideoAvailable() )
+        {
+            fullyWatched = false;
+            reward = null;
+
+            incentivizedInterstitial.show( parentActivity, null, this, this, this, this );
+        }
+        else
+        {
+            log( ERROR, "Failed to show an AppLovin rewarded video before one was loaded" );
+            MoPubRewardedVideoManager.onRewardedVideoPlaybackError( this.getClass(), "", MoPubErrorCode.VIDEO_PLAYBACK_ERROR );
+        }
+    }
+
+    @Override
+    protected boolean hasVideoAvailable()
+    {
+        return incentivizedInterstitial.isAdReadyToDisplay();
+    }
+
+    @Override
+    @Nullable
+    protected LifecycleListener getLifecycleListener() { return null; }
+
+    @Override
+    @NonNull
+    protected String getAdNetworkId() { return ""; }
+
+    @Override
+    protected void onInvalidate() {}
+
+    //
+    // Ad Load Listener
+    //
+
+    @Override
+    public void adReceived(final AppLovinAd ad)
+    {
+        log( DEBUG, "Rewarded video did load ad: " + ad.getAdIdNumber() );
+        MoPubRewardedVideoManager.onRewardedVideoLoadSuccess( this.getClass(), "" );
+    }
+
+    @Override
+    public void failedToReceiveAd(final int errorCode)
+    {
+        log( DEBUG, "Rewarded video failed to load with error: " + errorCode );
+        MoPubRewardedVideoManager.onRewardedVideoLoadFailure( this.getClass(), "", toMoPubErrorCode( errorCode ) );
+
+        // TODO: Add support for backfilling on regular ad request if invalid zone entered
+    }
+
+    //
+    // Ad Display Listener
+    //
+
+    @Override
+    public void adDisplayed(final AppLovinAd ad)
+    {
+        log( DEBUG, "Rewarded video displayed" );
+        MoPubRewardedVideoManager.onRewardedVideoStarted( this.getClass(), "" );
+    }
+
+    @Override
+    public void adHidden(final AppLovinAd ad)
+    {
+        log( DEBUG, "Rewarded video dismissed" );
+
+        if ( fullyWatched && reward != null )
+        {
+            log( DEBUG, "Rewarded" + reward.getAmount() + " " + reward.getLabel() );
+            MoPubRewardedVideoManager.onRewardedVideoCompleted( this.getClass(), "", reward );
+        }
+
+        MoPubRewardedVideoManager.onRewardedVideoClosed( this.getClass(), "" );
+    }
+
+    //
+    // Ad Click Listener
+    //
+
+    @Override
+    public void adClicked(final AppLovinAd ad)
+    {
+        log( DEBUG, "Rewarded video clicked" );
+        MoPubRewardedVideoManager.onRewardedVideoClicked( this.getClass(), "" );
+    }
+
+    //
+    // Video Playback Listener
+    //
+
+    @Override
+    public void videoPlaybackBegan(final AppLovinAd ad)
+    {
+        log( DEBUG, "Rewarded video playback began" );
+    }
+
+    @Override
+    public void videoPlaybackEnded(final AppLovinAd ad, final double percentViewed, final boolean fullyWatched)
+    {
+        log( DEBUG, "Rewarded video playback ended at playback percent: " + percentViewed );
+
+        this.fullyWatched = fullyWatched;
+    }
+
+    //
+    // Reward Listener
+    //
+
+    @Override
+    public void userOverQuota(final AppLovinAd appLovinAd, final Map map)
+    {
+        log( ERROR, "Rewarded video validation request for ad did exceed quota with response: " + map );
+    }
+
+    @Override
+    public void validationRequestFailed(final AppLovinAd appLovinAd, final int errorCode)
+    {
+        log( ERROR, "Rewarded video validation request for ad failed with error code: " + errorCode );
+    }
+
+    @Override
+    public void userRewardRejected(final AppLovinAd appLovinAd, final Map map)
+    {
+        log( ERROR, "Rewarded video validation request was rejected with response: " + map );
+    }
+
+    @Override
+    public void userDeclinedToViewAd(final AppLovinAd appLovinAd)
+    {
+        log( DEBUG, "User declined to view rewarded video" );
+        MoPubRewardedVideoManager.onRewardedVideoClosed( this.getClass(), "" );
+    }
+
+    @Override
+    public void userRewardVerified(final AppLovinAd appLovinAd, final Map map)
+    {
+        final String currency = (String) map.get( "currency" );
+        final int amount = (int) Double.parseDouble( (String) map.get( "amount" ) ); // AppLovin returns amount as double
+
+        log( DEBUG, "Verified " + amount + " " + currency );
+
+        reward = MoPubReward.success( currency, amount );
+    }
+
+    //
+    // Dynamically create an instance of AppLovinIncentivizedInterstitial with a given zone without breaking backwards compatibility for publishers on older SDKs.
+    //
+    private AppLovinIncentivizedInterstitial createIncentivizedInterstitialForZoneId(final String zoneId, final AppLovinSdk sdk)
+    {
+        AppLovinIncentivizedInterstitial incent = null;
+
+        try
+        {
+            final Method method = AppLovinIncentivizedInterstitial.class.getMethod( "create", String.class, AppLovinSdk.class );
+            incent = (AppLovinIncentivizedInterstitial) method.invoke( null, zoneId, sdk );
+        }
+        catch ( Throwable th )
+        {
+            log( ERROR, "Unable to load ad for zone: " + zoneId + "..." );
+            MoPubRewardedVideoManager.onRewardedVideoLoadFailure( getClass(), "", MoPubErrorCode.ADAPTER_CONFIGURATION_ERROR );
+        }
+
+        return incent;
+    }
+
+    //
+    // Utility Methods
+    //
+
+    private static void log(final int priority, final String message)
+    {
+        if ( LOGGING_ENABLED )
+        {
+            Log.println( priority, "AppLovinRewardedVideo", message );
+        }
+    }
+
+    private static MoPubErrorCode toMoPubErrorCode(final int applovinErrorCode)
+    {
+        if ( applovinErrorCode == AppLovinErrorCodes.NO_FILL )
+        {
+            return MoPubErrorCode.NETWORK_NO_FILL;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.UNSPECIFIED_ERROR )
+        {
+            return MoPubErrorCode.NETWORK_INVALID_STATE;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.NO_NETWORK )
+        {
+            return MoPubErrorCode.NO_CONNECTION;
+        }
+        else if ( applovinErrorCode == AppLovinErrorCodes.FETCH_AD_TIMEOUT )
+        {
+            return MoPubErrorCode.NETWORK_TIMEOUT;
+        }
+        else
+        {
+            return MoPubErrorCode.UNSPECIFIED;
+        }
+    }
+}
